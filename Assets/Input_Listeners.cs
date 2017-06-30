92e817e6aba180490d359d52039dd522fc63c515
mammoths <emailmichellevo@gmail.com>
2017-06-30 15:26:34 -0700

    Singleton Service and Inputs from SteamVR

---
+++ Assets/Input_Listeners.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Input_Listeners : MonoBehaviour {
+
+   public bool leftTriggerInteractive = false;
+   public bool rightTriggerInteractive = false;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    public bool getLeftTriggerInteracting()
+    {
+        return leftTriggerInteractive; 
+    }
+
+    public bool getRightTriggerInteracting()
+    {
+        return rightTriggerInteractive;
+    }
+
+
+   public  void setLeftTriggerInteracting(bool value)
+    {
+        
+        leftTriggerInteractive = value; 
+    }
+
+   public  void setRightTriggerInteracting(bool value)
+    {
+      
+        rightTriggerInteractive = value;
+    }
+
+    void OnEnable()
+    {
+        Singleton_Service.RegisterSingletonInstance(this);
+    }
+    void OnDisable()
+    {
+        Singleton_Service.UnregisterSingletonInstance(this);
+    }
+}
---
+++ Assets/Right_VR_Cont.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class Right_VR_Cont : MonoBehaviour
+{
+
+    private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+
+    private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int)trackedObj.index); } }
+    private SteamVR_TrackedObject trackedObj;
+
+    Input_Listeners IPL;
+    // Use this for initialization
+    void Start()
+    {
+        IPL = GameObject.Find("GM").GetComponent<Input_Listeners>();
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (controller.GetPressDown(triggerButton))
+        {
+            IPL.setRightTriggerInteracting(true);
+
+        }
+        if (controller.GetPressUp(triggerButton))
+        {
+            IPL.setRightTriggerInteracting(false);
+        }
+    }
+
+}
